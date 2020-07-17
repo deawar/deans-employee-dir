85162226cd97aa0cf89ca18c40051ff6a5d74fb6
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-07-17 15:49:43 -0400

    refactored the sort function to only handle name.first

--- src/components/DataBody.js
+++ src/components/DataBody.js
@@ -17,8 +17,8 @@ const DataBody = () => {
 
   return (
     <tbody>
-      {context.developerState.filteredUsers[0] !== undefined && context.developerState.filteredUsers[0].name !== undefined ? (
-        context.developerState.filteredUsers.map(({ login, name, picture, phone, email, dob }) => {
+      {context.filteredUsers[0] !== undefined && context.filteredUsers[0].name !== undefined ? (
+        context.filteredUsers.map(({ login, name, picture, phone, email, dob }) => {
           return (
             <tr key={login.uuid}>
               <td data-th="Image" className="align-middle">
